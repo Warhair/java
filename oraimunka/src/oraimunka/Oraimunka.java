a1ac4691b548fe625af1849784c73e9960304c36
Warhair <36608769+Warhair@users.noreply.github.com>
2023-02-22 11:56:51 +0100

    elsofeladatjavitva

--- oraimunka/src/oraimunka/Oraimunka.java
+++ oraimunka/src/oraimunka/Oraimunka.java
@@ -1,21 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package oraimunka;
 
-/**
- *
- * @author tegze
- */
+
 public class Oraimunka {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         
     }
+    
     private static void elso10SzamOsszege(){
         int osszeg = 0;
         int db = 0;
@@ -25,7 +18,9 @@ public class Oraimunka {
         }
         osszead(osszeg, db);
         }
+    
     private static void osszead(int a, int b){
+        System.out.printf("%d + %d =%d\n", a, b, a+b);
         System.out.printf("Az els? %d szám összege: %d\n", a, b);
         
             
